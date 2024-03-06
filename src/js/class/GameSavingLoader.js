9564388429cc778396151740bed8fa507c9eb23d
Vadim Roziznan <roziznan_v@mail.ru>
2024-03-06 19:48:44 +0300

    changed GameSavingLoader.js

--- src/js/class/GameSavingLoader.js
+++ src/js/class/GameSavingLoader.js
@@ -3,19 +3,12 @@ import json from "../funcs/parser.js";
 
 
 export default class GameSavingLoader {
-  static load() {
-    return new Promise((resolve, reject) => {
-      read()
-        .then((data) => {
-          return json(data);
-        })
-        .then((parsedData) => {
-          const saving = JSON.parse(parsedData);
-          resolve(saving);
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
+
+  static async load() {
+      const data = await read();
+      const value = await json(data);
+      const GameSaving = JSON.parse(value);
+      
+      return GameSaving;
   }
 }
